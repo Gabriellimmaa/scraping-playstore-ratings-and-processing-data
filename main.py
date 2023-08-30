47a6cabeeba5ef46c4db5976e52342e14ae47517
Gabriellimmaa <42157830+Gabriellimmaa@users.noreply.github.com>
2023-08-30 18:20:11 -0300

    feat/execute-all-star-ratings

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ from google_play_scraper import Sort
 from scraping import NLTK, PlayStore, Scraping
 
 if __name__ == '__main__':
-    scraper = Scraping(
-        playstore_config=PlayStore(
-            url='com.samsung.android.app.watchmanager',
-            lang='pt',
-            country='BR',
-            sort=Sort.MOST_RELEVANT,
-            filter_score_with=5,
-            count=100
-        ),
-        nltk_config=NLTK(
-            lang='portuguese',
-            passes=35,
-            num_topics=10
-        ),
-        filename=f"watchmanager"
-    )
 
-    scraper.get_matches_LDA()
-    scraper.get_matches_NLP()
-    scraper.save_result()
+    for rating in range(1, 6):
+        scraper = Scraping(
+            playstore_config=PlayStore(
+                url='com.samsung.android.app.watchmanager',
+                lang='pt',
+                country='BR',
+                sort=Sort.MOST_RELEVANT,
+                filter_score_with=rating,
+                count=500
+            ),
+            nltk_config=NLTK(
+                lang='portuguese',
+                passes=35,
+                num_topics=10
+            ),
+            filename=f"watchmanager"
+        )
+
+        scraper.get_matches_LDA()
+        scraper.get_matches_NLP()
+        scraper.save_result()
